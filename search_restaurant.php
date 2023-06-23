1ec861346c2ce3140bdcaf10543d06e693ed378c
Kofi Eric <ericp@menufinderafrica.com>
2023-06-23 04:04:12 +0100

    edited search_restaurant endpoint

--- search_restaurant.php
+++ search_restaurant.php
@@ -1,14 +1,11 @@
 <?php
-
-//Check if incoming request method is GET
-if ($_SERVER['REQUEST_METHOD'] != 'GET') {
-	
-	$status = array(
-                'code' => 101,
-                'status' => 'error',
-                'message' =>'Invalid Request. Must be a GET request'
-            );
-	echo json_encode($status);
+//Check if incoming request method is POST
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+	echo json_encode([
+        'code' => 101,
+        'status' => 'error',
+        'message' =>'Invalid Request. Must be a POST request'
+    ], JSON_PRETTY_PRINT);
 	exit();
 	
 }
@@ -19,51 +16,65 @@ else{
     if (!array_key_exists('authorization_code', $headers)) {
 
         echo json_encode([
-            "code" => 101,
-            "status" => "error",
-            "error" => "authorization_code header is missing"
-        ]);
+            'code' => 101,
+            'status' => 'error',
+            'message' =>'authorization_code header is missing'
+        ], JSON_PRETTY_PRINT);
+        
         exit;
     }
     elseif ( $headers['authorization_code'] !== base64_encode('foodDeliveryAPI@user') ) {
 
         echo json_encode([
-            "code" => 101,
-            "status" => "error",
-            "error" => "Incorrect authorization_code. It must be base64 encoded"
-        ]);
+            'code' => 101,
+            'status' => 'error',
+            'message' =>'Incorrect authorization_code. It must be base64 encoded'
+        ], JSON_PRETTY_PRINT);
+
         exit;
     }
     else{
 
     //Get request body parameters
     $request_body = file_get_contents('php://input'); 
-    $decoded_request_body =  json_decode($request_body,true);
     
-    if( !array_key_exists('search_term', $decoded_request_body) ){
+    if( !$request_body ){
         echo json_encode([
-            "code" => 101,
-            "status" => "error",
-            "message" => "search_term is missing."
-        ]);
-    }
-    elseif( empty($decoded_request_body['search_term']) || $decoded_request_body['search_term'] === ''){
-                echo json_encode([
-                    "code" => 101,
-                    "status" => "error",
-                    "message" => "search_term is empty."
-                ]);
-    }
+            'code' => 101,
+            'status' => 'error',
+            'message' =>'No request body specified'
+        ], JSON_PRETTY_PRINT);
+    } 
     else{
 
+        $decoded_request_body =  json_decode($request_body,true);
+
+        $search_term=filter_var($decoded_request_body['search_term'],FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH);
+
+    
+        if( !array_key_exists('search_term', $decoded_request_body) ){
             echo json_encode([
-                "code" => 200,
-                "status" => "success",
-                'message' =>'search term is '.$decoded_request_body['search_term']
-            ]);
-
-    }
-
+                'code' => 101,
+                'status' => 'error',
+                'message' =>'search_term is missing.'
+            ], JSON_PRETTY_PRINT);
+        } 
+        elseif( empty($search_term) || $search_term === ''){
+            echo json_encode([
+                'code' => 101,
+                'status' => 'error',
+                'message' =>'search_term is empty.'
+            ], JSON_PRETTY_PRINT);
+        }       
+        else{
+    
+            
+    
+        }
+    
+    
+        }
+    
 
     }
 
