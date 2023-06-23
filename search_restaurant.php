359872bcae9a5e1a9c9e1987d54a53e2197a22a4
Kofi Eric <ericp@menufinderafrica.com>
2023-06-23 06:54:34 +0100

    edited search_restaurant endpoint

--- search_restaurant.php
+++ search_restaurant.php
@@ -51,7 +51,6 @@ else{
 
         $search_term=filter_var($decoded_request_body['search_term'],FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH);
 
-    
         if( !array_key_exists('search_term', $decoded_request_body) ){
             echo json_encode([
                 'code' => 101,
@@ -68,7 +67,45 @@ else{
         }       
         else{
     
+            include_once('config/database_connection.php');
+
+            //Check if search_term matches any Restaurant name
+        
+            $checkForExistence = mysqli_query($conn, "SELECT rest_id, restaurant_name, restaurant_address, restaurant_city, restaurant_country, restaurant_phonenumber, restaurant_phonenumber FROM restaurants WHERE restaurant_name LIKE '%$search_term%' OR rest_id LIKE '%$search_term%'");
             
+            if( mysqli_num_rows($checkForExistence) > 0) {                
+
+                $restaurant_array = [];
+
+                while( $row =mysqli_fetch_assoc($checkForExistence) )
+                {
+                    $restaurant_array[]= $row;
+                    
+                    $rest_id=$row['rest_id'];
+
+                    $Get_Menus = mysqli_query($conn, "SELECT menu_id, restaurant_id, menu_item, menu_price, menu_description FROM menus WHERE restaurant_id ='$rest_id' ");
+
+                    $menu_array = [];
+                    while( $row_menu =mysqli_fetch_assoc($Get_Menus) )
+                    {
+                        $menu_array[]= $row_menu;
+                    }
+
+                }                
+
+                echo json_encode( array_merge($restaurant_array, $menu_array) , JSON_PRETTY_PRINT);
+
+                //close the db connection
+                mysqli_close($conn);
+
+
+            // echo json_encode([ "code"=> 101, "status"=> "success", "message"=> "Found"], JSON_PRETTY_PRINT);
+            }
+            else {
+
+                echo json_encode([ "code"=> 101, "status"=> "error", "message"=> "No Result found"], JSON_PRETTY_PRINT);
+
+            }
     
         }
     
